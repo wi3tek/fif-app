c01ec91fc9073c30b9640cb655d93e80b7c15802
wi3tek <patrykwieteskapw@gmail.com>
2020-01-30 18:03:57 +0100

    Initial commit

---
+++ src/main/java/pl/engineerproject/pw/fifapp/FifAppApplication.java
@@ -0,0 +1,13 @@
+package pl.engineerproject.pw.fifapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/engineerproject/pw/fifapp/FifAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.engineerproject.pw.fifapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
