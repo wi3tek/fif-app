18170708eada602c376a5bace4616af0d7beaa3d
wi3tek <patrykwieteskapw@gmail.com>
2020-02-18 22:36:43 +0100

    add simple routing (leagues/rounds)

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -1,6 +1,26 @@
 //'use strict'
 
-var fifapp = angular.module('fifapp', ['league.controllers', 'league.services']);
+var fifapp = angular.module('fifapp', ['ngRoute', 'league.controllers', 'league.services', 'round.controllers', 'round.services']);
+
+fifapp.config(function($routeProvider) {
+    $routeProvider
+
+        .when('/leagues', {
+            templateUrl: '../../view/League/view.html',
+            controller: 'LeagueController'
+        })
+        .when('/leagues/:leagueId', {
+            url: '/leagues/:leagueId',
+            templateUrl: '../../view/Round/view.html',
+            controller: 'RoundController',
+        })
+
+    .otherwise({
+        redirectTo: '/'
+    })
+
+
+});
 
 fifapp.constant("CONSTANTS", {
     getLeagueByIdUrl: "/leagues/getLeague",
