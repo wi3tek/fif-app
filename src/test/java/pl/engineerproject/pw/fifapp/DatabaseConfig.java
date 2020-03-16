52829a350c6c8ff726bd7ea27d980d0f3b45e988
wi3tek <patrykwieteskapw@gmail.com>
2020-03-16 18:19:27 +0100

    change db properties

---
+++ src/test/java/pl/engineerproject/pw/fifapp/DatabaseConfig.java
@@ -0,0 +1,20 @@
+package pl.engineerproject.pw.fifapp;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
