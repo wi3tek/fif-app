36db7d6744ff9b02dc00f3c8f9a20510e7f8f7c7
wi3tek <patrykwieteskapw@gmail.com>
2020-03-17 19:07:50 +0100

    fix urls to https

--- src/main/java/pl/engineerproject/pw/fifapp/DatabaseConfig.java
+++ src/main/java/pl/engineerproject/pw/fifapp/DatabaseConfig.java
@@ -7,12 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-
-
+@Configuration
 public class DatabaseConfig {
     @Value("${spring.datasource.url}")
     private String dbUrl;
-
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
